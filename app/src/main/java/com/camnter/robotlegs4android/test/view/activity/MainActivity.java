e4e87984b2a8cbd8b32ff86cab1eabd7df8bb3b5
CaMnter <421482590@qq.com>
2015-12-31 12:16:16 +0800

    add license

--- app/src/main/java/com/camnter/robotlegs4android/test/view/activity/MainActivity.java
+++ app/src/main/java/com/camnter/robotlegs4android/test/view/activity/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 CaMnter 421482590@qq.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.camnter.robotlegs4android.test.view.activity;
 
 import android.os.Bundle;
@@ -5,7 +21,11 @@ import android.os.Bundle;
 import com.camnter.robotlegs4android.test.R;
 import com.camnter.robotlegs4android.views.RobotlegsFragmentActivity;
 
-
+/**
+ * Description：MainActivityMediator
+ * Created by：CaMnter
+ * Time：2015-11-07 23:39
+ */
 public class MainActivity extends RobotlegsFragmentActivity {
 
     /**
